ea980a5532788160ad4150ea19e90bb6bae92d71
PCE <pete.earle@gmail.com>
2016-11-20 19:19:31 -0500

    Add files via upload

--- PA1_template.R
+++ PA1_template.R
@@ -1,108 +1,84 @@
-
-Reproducible Research, Project 1
-================================
-
-```{r, echo=FALSE}
 library(ggplot2)
 library(plyr)
 
+#activity <- read.csv("C:\Users\Owner\datasciencecoursera\activity.csv")
 activity <- read.csv("C://Users/Owner/datasciencecoursera/activity.csv")
 
 activity$day <- weekdays(as.Date(activity$date))
 activity$DateTime<- as.POSIXct(activity$date, format="%Y-%m-%d")
+
+##pulling data without nas
 clean <- activity[!is.na(activity$steps),]
 
+## summarizing total steps per date
 sumTable <- aggregate(activity$steps ~ activity$date, FUN=sum, )
 colnames(sumTable)<- c("Date", "Steps")
-```
 
-##Histogram, total steps per day
-```{r, echo=FALSE}
+## Creating the historgram of total steps per day
 hist(sumTable$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day")
-```
 
-##Mean of Steps
-```{r, echo=FALSE}
+## Mean of Steps
 as.integer(mean(sumTable$Steps))
-```
 
-##Median of Steps
-```{r, echo=FALSE}
+## Median of Steps
 as.integer(median(sumTable$Steps))
-```
 
-
-```{r, echo=FALSE}
-##pulling data
 library(plyr)
 library(ggplot2)
+##pulling data without nas
 clean <- activity[!is.na(activity$steps),]
+
+##create average number of steps per interval
 intervalTable <- ddply(clean, .(interval), summarize, Avg = mean(steps))
-```
 
-
-```{r, echo=FALSE}
+##Create line plot of average number of steps per interval
 p <- ggplot(intervalTable, aes(x=interval, y=Avg), xlab = "Interval", ylab="Average Number of Steps")
 p + geom_line()+xlab("Interval")+ylab("Average Number of Steps")+ggtitle("Average Number of Steps per Interval")
-```
 
 ##Maximum steps by interval
-```{r, echo=FALSE}
 maxSteps <- max(intervalTable$Avg)
+##Which interval contains the maximum average number of steps
 intervalTable[intervalTable$Avg==maxSteps,1]
-```
 
-##Total number of missing values ("NA")
-```{r, echo=FALSE}
+##Number of NAs in original data set
 nrow(activity[is.na(activity$steps),])
-```
 
-```{r, echo=FALSE}
+## Create the average number of steps per weekday and interval
 avgTable <- ddply(clean, .(interval, day), summarize, Avg = mean(steps))
 
+## Create dataset with all NAs for substitution
 nadata<- activity[is.na(activity$steps),]
-
+## Merge NA data with average weekday interval for substitution
 newdata<-merge(nadata, avgTable, by=c("interval", "day"))
 
+## Reorder the new substituded data in the same format as clean data set
 newdata2<- newdata[,c(6,4,1,2,5)]
-
 colnames(newdata2)<- c("steps", "date", "interval", "day", "DateTime")
 
+##Merge the NA averages and non NA data together
 mergeData <- rbind(clean, newdata2)
-```
 
-##Mean of steps with NA data
-```{r, echo=FALSE}
+##Create sum of steps per date to compare with step 1
 sumTable2 <- aggregate(mergeData$steps ~ mergeData$date, FUN=sum, )
 colnames(sumTable2)<- c("Date", "Steps")
+
+## Mean of Steps with NA data taken care of
 as.integer(mean(sumTable2$Steps))
-```
 
-##Median of steps with NA data
-```{r, echo=FALSE}
-as.integer(median(sumTable2$Steps))
-```
-
-##Histogram, total steps per day
-```{r, echo=FALSE}
+## Creating the histogram of total steps per day, categorized by data set to show impact
 hist(sumTable2$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with NAs Fixed", col="Black")
 hist(sumTable$Steps, breaks=5, xlab="Steps", main = "Total Steps per Day with NAs Fixed", col="Grey", add=T)
 legend("topright", c("Imputed Data", "Non-NA Data"), fill=c("black", "grey") )
-```
 
-
-```{r, echo=FALSE}
+## Create new category based on the days of the week
 mergeData$DayCategory <- ifelse(mergeData$day %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
+
 library(lattice) 
-```
 
-```{r, echo=FALSE}
+## Summarize data by interval and type of day
 intervalTable2 <- ddply(mergeData, .(interval, DayCategory), summarize, Avg = mean(steps))
-```
 
-##Panel plot
-```{r, echo=FALSE}
+##Plot data in a panel plot
 xyplot(Avg~interval|DayCategory, data=intervalTable2, type="l",  layout = c(1,2),
        main="Average Steps per Interval Based on Type of Day", 
        ylab="Average Number of Steps", xlab="Interval")
-```
